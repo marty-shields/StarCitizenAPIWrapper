7f5e040d7d23e6d7b7300d4c8f89667baf93e500
TheAnachronism <Tobias.Groeli@Outlook.com>
2020-09-25 15:06:11 +0200

    Added the console testing project.

---
+++ StarCitizenAPIWrapper/Examples/StarCitizenAPIWrapper.ConsoleTesting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using StarCitizenAPIWrapper.Library;
+
+namespace StarCitizenAPIWrapper.ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            var client = StarCitizenClient.GetClient();
+            var user = await client.GetUser("Anachronis");
+        }
+    }
+}
