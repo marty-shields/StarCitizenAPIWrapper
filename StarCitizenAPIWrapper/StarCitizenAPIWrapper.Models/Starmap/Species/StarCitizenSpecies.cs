a70cb243fd3c54b1636e5c5dea23a470dcf99b7c
TheAnachronism <Tobias.Groeli@Outlook.com>
2020-09-30 00:10:06 +0200

    Added the species model for the species API endpoint

---
+++ StarCitizenAPIWrapper/StarCitizenAPIWrapper.Models/Starmap/Species/StarCitizenSpecies.cs
@@ -0,0 +1,22 @@
+﻿namespace StarCitizenAPIWrapper.Models.Starmap.Species
+{
+    /// <summary>
+    /// The information about a species found in the star citizen verse.
+    /// </summary>
+    public class StarCitizenSpecies
+    {
+        /// <summary>
+        /// The code of this species.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// The id of this species.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The name of this species.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
