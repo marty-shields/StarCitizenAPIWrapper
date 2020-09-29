55eb9ee00633eb0ec7ae7ae800f939941c2497a5
TheAnachronism <Tobias.Groeli@Outlook.com>
2020-09-29 17:21:00 +0200

    Added the roadmap models in the .Models library

---
+++ StarCitizenAPIWrapper/StarCitizenAPIWrapper.Models/RoadMap/RoadMapCardThumbnail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace StarCitizenAPIWrapper.Models.RoadMap
+{
+    /// <summary>
+    /// The information of a thumbnail of a roadmap card.
+    /// </summary>
+    public class RoadMapCardThumbnail
+    {
+        /// <summary>
+        /// The id of the roadmap card thumbnail.
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// The image urls of this thumbnail.
+        /// </summary>
+        public Dictionary<string, string> Urls { get; set; }
+    }
+}
