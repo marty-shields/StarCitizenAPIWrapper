6b4f0a9f0e7bc860def5420afc4412562cf9cf92
TheAnachronism <Tobias.Groeli@Outlook.com>
2020-09-29 20:03:28 +0200

    Added the starmap systems models.

---
+++ StarCitizenAPIWrapper/StarCitizenAPIWrapper.Models/Starmap/Systems/StarmapSystem.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using StarCitizenAPIWrapper.Models.Attributes;
+using System.Collections.Generic;
+
+namespace StarCitizenAPIWrapper.Models.Starmap.Systems
+{
+    /// <summary>
+    /// The information about a starmap system from RSI.
+    /// </summary>
+    public class StarmapSystem
+    {
+        /// <summary>
+        /// The different affiliations of this system.
+        /// </summary>
+        [ApiName("affiliation")]
+        public List<StarmapSystemAffiliation> Affiliations { get; set; }
+        /// <summary>
+        /// The aggregated danger of this system.
+        /// </summary>
+        [ApiName("aggregated_danger")]
+        public int AggregatedDanger { get; set; }
+        /// <summary>
+        /// The aggregated economy of this system.
+        /// </summary>
+        [ApiName("aggregated_economy")]
+        public int AggregatedEconomy { get; set; }
+        /// <summary>
+        /// The aggregated population of this system.
+        /// </summary>
+        [ApiName("aggregated_population")]
+        public int AggregatedPopulation { get; set; }
+        /// <summary>
+        /// The aggregated size of this system.
+        /// </summary>
+        [ApiName("aggregated_size")]
+        public double AggregatedSize { get; set; }
+        /// <summary>
+        /// The code of this system.
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// The description of this system.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// The id of this system.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The info url of this system.
+        /// </summary>
+        [ApiName("info_url")]
+        public string InfoUrl { get; set; }
+        /// <summary>
+        /// The name of this system.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The x position of this system in the verse.
+        /// </summary>
+        [ApiName("position_x")]
+        public double PositionX { get; set; }
+        /// <summary>
+        /// The y position of this system in the verse.
+        /// </summary>
+        [ApiName("position_y")]
+        public double PositionY { get; set; }
+        /// <summary>
+        /// The z position of this system in the verse.
+        /// </summary>
+        [ApiName("position_z")]
+        public double PositionZ { get; set; }
+        /// <summary>
+        /// The status of this system.
+        /// </summary>
+        public char Status { get; set; }
+        /// <summary>
+        /// The thumbnail information of this system.
+        /// </summary>
+        public StarmapSystemThumbnail Thumbnail { get; set; }
+        /// <summary>
+        /// The last time this system was modified.
+        /// </summary>
+        [ApiName("time_modified")]
+        public DateTime TimeModified { get; set; }
+        /// <summary>
+        /// The system type.
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
---
+++ StarCitizenAPIWrapper/StarCitizenAPIWrapper.Models/Starmap/Systems/StarmapSystemAffiliation.cs
@@ -0,0 +1,32 @@
+﻿using StarCitizenAPIWrapper.Models.Attributes;
+
+namespace StarCitizenAPIWrapper.Models.Starmap.Systems
+{
+    /// <summary>
+    /// The information about an affiliation of a starmap system/
+    /// </summary>
+    public class StarmapSystemAffiliation
+    {
+        /// <summary>
+        /// The code of the affiliated instance.
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// The color of the affiliated instance.
+        /// </summary>
+        public string Color { get; set; }
+        /// <summary>
+        /// The id of this affiliation.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The id of the membership of the affiliated instance.
+        /// </summary>
+        [ApiName("membership_id")]
+        public int MembershipId { get; set; }
+        /// <summary>
+        /// The name of the affiliated instance.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
