870349ce624673c911f9dea2e903bd559d7f2fa0
TheAnachronism <Tobias.Groeli@Outlook.com>
2020-09-29 21:38:29 +0200

    Added starmap system tunnel models.

---
+++ StarCitizenAPIWrapper/StarCitizenAPIWrapper.Models/Starmap/Tunnels/StarmapTunnel.cs
@@ -0,0 +1,33 @@
+﻿namespace StarCitizenAPIWrapper.Models.Starmap.Tunnels
+{
+    /// <summary>
+    /// The information about a tunnel inside of a star system.
+    /// </summary>
+    public class StarmapTunnel
+    {
+        /// <summary>
+        /// The direction of this tunnel.
+        /// </summary>
+        public char Direction { get; set; }
+        /// <summary>
+        /// The two entries this tunnel has.
+        /// </summary>
+        public (StarmapTunnelEntry, StarmapTunnelEntry ) Entry { get; set; }
+        /// <summary>
+        /// The id of the exit of this tunnel.
+        /// </summary>
+        public int ExitId { get; set; }
+        /// <summary>
+        /// The id of this tunnel.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The name of this tunnel.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The size of this tunnel.
+        /// </summary>
+        public char Size { get; set; }
+    }
+}
